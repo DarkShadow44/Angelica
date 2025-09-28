4f6cb9263d5f2d8a98725b8007d54a727f8266fd
Fabian Maurer <dark.shadow4@web.de>
2025-09-28 05:40:51 +0200

    fixes

--- src/main/java/com/gtnewhorizons/angelica/ao/AOHelper.java
+++ src/main/java/com/gtnewhorizons/angelica/ao/AOHelper.java
@@ -1,6 +1,8 @@
 package com.gtnewhorizons.angelica.ao;
 
 import com.gtnewhorizon.gtnhlib.blockpos.BlockPos;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.util.ForgeDirection;
 import org.embeddedt.embeddium.impl.model.quad.properties.ModelQuadFacing;
@@ -16,7 +18,7 @@ public class AOHelper {
             case NEG_Y -> ForgeDirection.DOWN;
             case POS_Z -> ForgeDirection.SOUTH;
             case NEG_Z -> ForgeDirection.NORTH;
-            case UNASSIGNED -> ForgeDirection.UNKNOWN;
+            case UNASSIGNED -> ForgeDirection.UP; // TODO
         };
     }
     private static final ThreadLocal<EnhancedAoRenderStorage> AORenderer = ThreadLocal.withInitial(EnhancedAoRenderStorage::new);
@@ -24,17 +26,34 @@ public class AOHelper {
 
 
     public static void updateAO(ChunkVertexEncoder.Vertex[] celeritasVertices, IBlockAccess world, BlockPos pos, ModelQuadFacing facing) {
-        var ao = AORenderer.get();
+        var ao = new EnhancedAoRenderStorage();
         var quad  = QUAD.get();
-        quad.fill(celeritasVertices);
+        quad.fill(celeritasVertices, pos);
         ForgeDirection direction = facingToDirection(facing);
 
         ao.calculateShape(world, new FakeBlockState(world, pos), pos, quad.vertices, direction);
         ao.captureQuad(quad);
         ao.calculate(world, new FakeBlockState(world, pos), pos, direction, true);
 
+        Block block = world.getBlock(pos.x, pos.y, pos.z);
+
         for (int i = 0; i < 4; i++) {
-            celeritasVertices[i].light = ao.lightmap[i];
+            int l = block.colorMultiplier(world, pos.x, pos.y, pos.z);
+            float r = (float)(l >> 16 & 255) / 255.0F;
+            float g = (float)(l >> 8 & 255) / 255.0F;
+            float b = (float)(l & 255) / 255.0F;
+            int a = celeritasVertices[i].color & 0xFF000000;
+            float brightness = ao.brightness[i];
+            r *= brightness;
+            g *= brightness;
+            b *= brightness;
+
+            int color = a |
+                ((int)(r * 255) << 16) |
+                ((int)(g * 255) << 8)  |
+                ((int)(b * 255));
+
+            celeritasVertices[i].color = color;
         }
     }
 }
--- src/main/java/com/gtnewhorizons/angelica/ao/BakedQuad.java
+++ src/main/java/com/gtnewhorizons/angelica/ao/BakedQuad.java
@@ -1,15 +1,23 @@
 package com.gtnewhorizons.angelica.ao;
 
+import com.gtnewhorizon.gtnhlib.blockpos.BlockPos;
 import org.embeddedt.embeddium.impl.render.chunk.vertex.format.ChunkVertexEncoder;
 
 public class BakedQuad {
     public final float[] vertices = new float[4 * 3];
 
-    public void fill(ChunkVertexEncoder.Vertex[] celeritasVertices) {
+    public void fill(ChunkVertexEncoder.Vertex[] celeritasVertices, BlockPos pos) {
+        int offsetX = pos.x % 16;
+        int offsetY = pos.y % 16;
+        int offsetZ = pos.z % 16;
+
+        if (offsetX < 0) offsetX += 16;
+        if (offsetY < 0) offsetY += 16;
+        if (offsetZ < 0) offsetZ += 16;
         for (int i = 0; i < 4; i++) {
-            vertices[i*3] = celeritasVertices[i].x;
-            vertices[i*3 + 1] = celeritasVertices[i].y;
-            vertices[i*3 + 2] = celeritasVertices[i].z;
+            vertices[i*3] = celeritasVertices[i].x - offsetX;
+            vertices[i*3 + 1] = celeritasVertices[i].y - offsetY;
+            vertices[i*3 + 2] = celeritasVertices[i].z - offsetZ;
         }
     }
 
